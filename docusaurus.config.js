ed193c4866ce9f6a6a9dd5eb46ab00f8a38ecd39
Rafaf Tahsin <rafaf.tahsin@infolytx.com>
2023-12-13 15:13:46 +0600

    updated docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://rafaftahsin.github.io/rtx',
+  url: 'https://rafaftahsin.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/rtx',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
